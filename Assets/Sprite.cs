9a1500d0f09f487e5499d2fec9c08655089587bd
moham187 <moham187@OA-S213-05>
2026-03-18 02:51:46 -0400

    Coding gym 9

---
+++ Assets/Sprite.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public class Sprite : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    public int spriteIndex = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeSprite()
+    {
+        spriteIndex += 1;
+
+        spriteRenderer.sprite = sprites[spriteIndex];
+
+    }
+}
