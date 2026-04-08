a7a56b27670ef4ed45a385cee17b239697eef0c4
moham187 <moham187@OA-S213-06>
2026-04-07 20:45:09 -0400

    fixed

---
+++ Assets/Assignment/Honeycomb.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Honeycomb : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.position = new Vector3 (0, 9, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
