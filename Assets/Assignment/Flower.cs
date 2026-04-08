c4a2b90228ebc6d1c1793a719a24b2bdac9233fc
moham187 <moham187@OA-S213-06>
2026-04-07 20:42:16 -0400

    Flower bloom animation added... done!

--- Assets/Assignment/Flower.cs
+++ Assets/Assignment/Flower.cs
@@ -8,28 +8,41 @@ public class Flower : MonoBehaviour
 {
     public float increase;
     public Slider pollinationbar;
+    public SpriteRenderer thisSprite;
+    public Sprite Bloom;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //assigns the spriterender to blooming flower variable
+        thisSprite = GetComponent<SpriteRenderer>();
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        //our slider value changes to the value of our variable
         pollinationbar.value = increase;
     }
-
+    //function that increases pollination bar when space is pressed...
     public void Pollinate()
     {
-
         if (increase  <= 1)
         {
             increase += 0.15f;
         }
+        onBloom();
     }
 
-    //function that increases pollination bar when space is pressed...
+    public void onBloom()
+    {
+        if(increase >= 1)
+        {
+            thisSprite.sprite = Bloom;
+        }
+    }
+   
     
 }
